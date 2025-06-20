a3a2cd9e70b8b03f1e0a328ffe165addd66fd5ee
nhien261203 <dovannhien12345@gmail.com>
2025-06-20 16:14:39 +0700

    make db migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('gender')->nullable();
             $table->timestamps();
         });
     }
